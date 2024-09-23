962cdc0c3569c458ff64a0478e69da35598b47a4
Wong Kai Li <53716255+garywongkai@users.noreply.github.com>
2024-09-23 13:01:51 +0800

    Update Signup.tsx

--- peerprep/src/pages/Signup.tsx
+++ peerprep/src/pages/Signup.tsx
@@ -33,46 +33,6 @@ const Signup: React.FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user, loading]);
   return (
-    // <ThemeProvider theme={theme}>
-    // <Header />
-    // <div className="register">
-    //   <div className="register__container">
-    //     <input
-    //       type="text"
-    //       className="register__textBox"
-    //       value={name}
-    //       onChange={(e) => setName(e.target.value)}
-    //       placeholder="Full Name"
-    //     />
-    //     <input
-    //       type="text"
-    //       className="register__textBox"
-    //       value={email}
-    //       onChange={(e) => setEmail(e.target.value)}
-    //       placeholder="E-mail Address"
-    //     />
-    //     <input
-    //       type="password"
-    //       className="register__textBox"
-    //       value={password}
-    //       onChange={(e) => setPassword(e.target.value)}
-    //       placeholder="Password"
-    //     />
-    //     <button className="register__btn" onClick={register}>
-    //       Register
-    //     </button>
-    //     <button
-    //       className="register__btn register__google"
-    //       onClick={signInWithGoogle}
-    //     >
-    //       Register with Google
-    //     </button>
-    //     <div>
-    //       Already have an account? <Link to="/signin">Login</Link> now.
-    //     </div>
-    //   </div>
-    // </div>
-    // </ThemeProvider>
     <ThemeProvider theme={theme}>
     <Header />
     <div className="register">
