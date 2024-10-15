724109ac00c66b5027cefaf18263ee7b02c4827e
ybq22 <ybq22@mails.tsinghua.edu.cn>
2024-10-15 20:23:10 +0800

    Revert "v2"
    This reverts commit 40ffe24da28928c55cf0ff53203abe73146162a5.

--- matching-service/server.js
+++ matching-service/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", matchingRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => {
   console.log(`Matching Service is running on port ${PORT}`);
 });
