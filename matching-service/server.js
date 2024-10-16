cd7f982486d267a2bafb3f698ab053c5aa600ddd
ybq22 <ybq22@mails.tsinghua.edu.cn>
2024-10-16 11:36:42 +0800

    some little changes

--- matching-service/server.js
+++ matching-service/server.js
@@ -9,7 +9,6 @@ app.use(bodyParser.json());
 
 const PORT = 5002;
 
-// CORS 配置
 const corsOptions = {
   origin: [
     "http://localhost:3000",
@@ -18,25 +17,24 @@ const corsOptions = {
     "https://peerprep-327190433280.asia-southeast1.run.app:3000",
     "https://peerprep-327190433280.asia-southeast1.run.app:5000",
   ],
-  optionsSuccessStatus: 200, // 适配旧版浏览器
+  optionsSuccessStatus: 200, // adaptive to old browser
 };
 
-// 启用 CORS
 app.use(cors(corsOptions));
-app.use(router);
+app.use("/match", router);
 
-// 根路由，检查服务器是否正常工作
+// root router
 app.get("/", (req, res) => {
   res.json({ message: "Matching Service is running!" });
 });
 
-// 错误处理
+// handle error 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
 
-// 启动服务器
+// start service
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
