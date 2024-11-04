2a151c45e4c7c16b0ec0ccd3a78e968314221f13
yap-zong-xin <yapzongxin@hotmail.com>
2024-11-05 03:35:31 +0800

    Update socket.js to localStorage

--- peerprep/src/pages/Collaboration/socket.js
+++ peerprep/src/pages/Collaboration/socket.js
@@ -1,37 +1,36 @@
-import io from 'socket.io-client';
-import { getCookie } from '../../utils/cookieUtils'; // 假设 getCookie 函数放在这个文件中
+import io from "socket.io-client";
 
-const cookieToken = getCookie('access_token');
-const displayName = getCookie('displayName');
-const selectedCategory = getCookie('selectedCategory');
-const selectedDifficulty = getCookie('selectedDifficulty');
-const uid = getCookie('uid');
+const accessToken = localStorage.getItem("accessToken");
+const displayName = localStorage.getItem("displayName");
+const selectedCategory = localStorage.getItem("selectedCategory");
+const selectedDifficulty = localStorage.getItem("selectedDifficulty");
+const uid = localStorage.getItem("uid");
 
-if (cookieToken) {
-    console.log("Token obtained from cookies");
+if (accessToken) {
+  console.log("Token obtained from localStorage");
 } else {
-    console.log("User is not authorized yet");
+  console.log("User is not authorized yet");
 }
 
-
-const URL = process.env.REACT_APP_ENV === "development"
-? "http://localhost:5003"
-: "https://collaboration-service-327190433280.asia-southeast1.run.app";;
+const URL =
+  process.env.REACT_APP_ENV === "development"
+    ? "http://localhost:5003"
+    : "https://collaboration-service-327190433280.asia-southeast1.run.app";
 
 export const socket = io(URL, {
-    query: {
-        token: cookieToken,  // access_token
-        displayName: displayName,  // User's display name
-        selectedCategory: selectedCategory,  // Selected category
-        selectedDifficulty: selectedDifficulty,  // Selected difficulty
-        uid: uid  // User ID
-    }
+  query: {
+    token: accessToken, // access_token
+    displayName: displayName, // User's display name
+    selectedCategory: selectedCategory, // Selected category
+    selectedDifficulty: selectedDifficulty, // Selected difficulty
+    uid: uid, // User ID
+  },
 });
 
-socket.on('connect', () => {
-    console.log('Socket connected successfully. Socket ID:', socket.id);
-    console.log('Socket connected successfully. Socket ID:', socket.displayName);
+socket.on("connect", () => {
+  console.log("Socket connected successfully. Socket ID:", socket.id);
+  console.log("Socket connected successfully. Socket ID:", socket.displayName);
 });
-socket.on('connect_error', (error) => {
-    console.log('Socket connection failed:', error);
+socket.on("connect_error", (error) => {
+  console.log("Socket connection failed:", error);
 });
