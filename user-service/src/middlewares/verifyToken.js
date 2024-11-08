824bd784394ec933a170589b7d9594ef39e54dee
yap-zong-xin <yapzongxin@hotmail.com>
2024-11-08 15:10:08 +0800

    Update token 4

--- user-service/src/middlewares/verifyToken.js
+++ user-service/src/middlewares/verifyToken.js
@@ -1,13 +1,15 @@
 const admin = require("firebase-admin");
 
 const verifyToken = async (req, res, next) => {
-    const token = req.headers.authorization?.split(" ")[1];
+    const authHeader = req.headers.authorization;
 
-    if (!token) {
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
         return res
             .status(401)
-            .json({ error: "Authentication token is missing" });
+            .json({ error: "Authentication token is missing or malformed" });
     }
+
+    const token = authHeader.split(" ")[1];
 
     try {
         const decodedToken = await admin.auth().verifyIdToken(token);
